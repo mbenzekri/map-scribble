d0a4a9a1d43274313fca590ef6e02949207f3454
mbenzekri <benzekri.mohamed@gmail.com>
2020-03-14 02:26:09 +0100

    code refactoring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import 'ol/ol.css';
 import { getUid } from 'ol/util.js';
-import Map from 'ol/Map';
+import { Map as olMap } from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import ImageLayer from 'ol/layer/Image';
@@ -38,41 +38,87 @@ CanvasTileLayerRenderer.prototype.drawTileImage = function (tile, frameState, x,
 const xyzurl = 'https://mbenzekri.github.io/fr-communes/fr/communes'
 const mapcolor = new window.Map()
 
-function initScribble() {
+class ThematicLayer extends ImageLayer {
+    /**
+     * 
+     * @param {string} xyzurl 
+     * @param {function} p2k 
+     * @param {Map<string,any>} mapcolor 
+     */
+    constructor(xyzurl, p2k, mapcolor) {
+        this.mapcolor = mapcolor
+        this.pixel2key = p2k
+        super({
+            source: new Raster({
+                sources: [new XYZ({
+                    url: xyzurl + '/{z}/{x}/{y}.png',
+                    maxZoom: 12,
+                    minZoom: 5,
+                    crossOrigin: 'anonymous',
+                    tileSize: 256,
+                    transition: 0,
+                    opaque: false
+                })],
+                operation: (pixels, data) => {
+                    const pixel = pixels[0]
+                    if (pixel[3] === 0) return pixel
+                    const key = this.pixel2key(pixel)
+                    let props = mapcolor.get(key)
+                    return props ? props.color : pixel
+                },
+                threads: 0
+            }),
+            transitions: 0
+        })
+    }
+    static from(xyzurl) {
+        const promises = [ThematicLayer.loadfunc(xyzurl), ThematicLayer.loaddata(xyzurl)]
+        return Promise.all(promises)
+            .then(arr => new ThematicLayer(xyzurl, arr[0], arr[1]))
+            .catch(err => {
+                console.error(`Unable to construct ThematicLayer due to :${err.message}`)
+                return Promise.resolve()
+            })
+    }
+    static loadfunc(xyzurl) {
+        return new Promise((resolve, reject) => {
+            fetch(xyzurl + '.js')
+                .then(resp => resp.text())
+                .then(fctxt => {
+                    try {
+                        const func = eval(`(${fctxt})`)
+                        const type = typeof func
+                        if (type === 'function') return resolve(func)
+                        reject(new Error(`${xyzurl}.js must provide a function. ${type} was provided`))
+                    } catch (e) {
+                        reject(new Error(`${xyzurl}.js execution fail due to : ${e.message}`))
+                    }
+                })
+        })
+    }
+    static loaddata(xyzurl) {
+        return fetch(xyzurl + '.json')
+            .then(resp => resp.json())
+            .then(data => {
+                const mapcolor = new window.Map()
+                if (!Array.isArray(data)) throw new Error(`loaded data is not an array`)
+                data.forEach(kvp => mapcolor.set(kvp.key, { color: null, name: kvp.name }))
+                return mapcolor
+            })
+            .catch(e => {
+                throw new Error(`${xyzurl}.json load fail due to : ${e.message}`)
+            })
+    }
+}
 
-    const xyz = new XYZ({
-        url: xyzurl + '/{z}/{x}/{y}.png',
-        maxZoom: 12,
-        minZoom: 5,
-        crossOrigin: 'anonymous',
-        tileSize: 256,
-        transition: 0,
-        opaque: false
-    })
-
-    const rastersource = new Raster({
-        sources: [xyz],
-        operation: function (pixels, data) {
-            const pixel = pixels[0]
-            if (pixel[3] === 0) return pixel
-            const key = pixel2key(pixel)
-            let props = mapcolor.get(key)
-            return props ? props.color : pixel
-        },
-        threads: 0
-    })
-
-    const thematiclayer = new ImageLayer({
-        source: rastersource,
-        transition: 0
-    })
+function initScribble(thematiclayer) {
 
     const debuglayer = new TileLayer({
         source: new TileDebug(),
         visible: false
     })
 
-    const map = new Map({
+    const map = new olMap({
         layers: [
             new TileLayer({
                 source: new OSM()
@@ -88,9 +134,6 @@ function initScribble() {
         })
     })
 
-    thematiclayer.on('prerender', evt => {
-    })
-
     const debugcheck = document.getElementById('debugcheck')
     const thematiccheck = document.getElementById('thematiccheck')
 
@@ -100,35 +143,17 @@ function initScribble() {
     thematiccheck.checked = thematiclayer.getVisible()
 }
 
-function load() {
-    return new Promise((resolve, reject) => {
-        var script = document.createElement('script');
-        script.onload = function (evt) {
-            resolve(evt)
-        };
-        script.onerror = function (evt) {
-            reject(evt)
-        };
-        script.src = xyzurl + '.js';
-        document.head.appendChild(script)
+ThematicLayer.from(xyzurl)
+    .then(thlayer => {
+        if (thlayer) {
+            thlayer.mapcolor.forEach((v, k) => {
+                const r = Math.ceil(Math.random() * 256)
+                const g = Math.ceil(Math.random() * 256)
+                const b = Math.ceil(Math.random() * 256)
+                v.color = [r, g, b, 255]
+            })
+            initScribble(thlayer)
+        }
     })
-}
 
-load()
-    .then(evt => {
-        console.log(evt)
-        return fetch(xyzurl+'.json').then(resp => resp.json())
-    }).then((data) => {
-        data.forEach(kvp => {
-            const r = Math.ceil(Math.random() * 256)
-            const g = Math.ceil(Math.random() * 256)
-            const b = Math.ceil(Math.random() * 256)
-            const color = [r, g, b, 255]
-            mapcolor.set(kvp.key, { color : color, name: kvp.name })
-        })
-        initScribble()
-    })
-    .catch(evt =>
-        console.log(evt)
-    )
 
